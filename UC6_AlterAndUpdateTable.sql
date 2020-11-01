dd29d3184356878dced44d20d605a792392366bb
CMD <gautamkailashsingh@gmail.com>
2020-11-01 19:14:15 +0530

    [Gautam] Alter and update the table to add gender column

---
+++ UC6_AlterAndUpdateTable.sql
@@ -0,0 +1,25 @@
+--Use Case 1
+create database payroll_service;
+use payroll_service;
+--Use Case 2
+create table employee_payroll
+(id int identity(1,1),
+ name varchar(25) not null,
+ salary money not null,
+ start_date date not null
+);
+--Use Case 3
+insert into employee_payroll values('Name1',454545,'2015-4-8');
+--Use case 4
+select * from employee_payroll;
+--Use Case 5
+insert into employee_payroll values('Name2',788787,'2045-4-7');
+insert into employee_payroll values('Name3',565656,'2016-5-04');
+select salary from employee_payroll where name='Name2';
+select * from employee_payroll where start_date between '2000-01-01' and GETDATE();
+--Use Case 6
+alter table employee_payroll add gender varchar(1);
+update employee_payroll set gender='M' where name='Name1';
+update employee_payroll set gender='F' where name='Name2';
+update employee_payroll set gender='T' where name='Name3';
+select * from employee_payroll;
