17aebe81c5b5c9812248107376865587c76b03bc
CMD <gautamkailashsingh@gmail.com>
2020-11-01 19:11:32 +0530

    [Gautam] Inserted data into the table

---
+++ UC3_InsertData.sql
@@ -0,0 +1,12 @@
+--Use Case 1
+create database payroll_service;
+use payroll_service;
+--Use Case 2
+create table employee_payroll
+(id int identity(1,1),
+ name varchar(25) not null,
+ salary money not null,
+ start_date date not null
+);
+--Use Case 3
+insert into employee_payroll values('Name1',454545,'2015-4-8');
